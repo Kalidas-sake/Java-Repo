e81dcf096d3b5aa41aaa0d9e7b142c9b0aeccd69
Kalidas-sake <kalidasake07@gmail.com>
2019-06-22 14:03:47 +0530

    overriding concept

---
+++ test.java
@@ -0,0 +1,26 @@
+package com.kali.basic;
+
+class Base {
+static void display() {
+System.out.println("Static or class method from Base");
+}
+public void print() {
+System.out.println("Non-static or instance method from Base");
+}
+}
+class Derived extends Base {
+ static void display() {
+System.out.println("Static or class method from Derived");
+}
+public void print() {
+System.out.println("Non-static or instance method from Derived");
+}
+}
+public class test {
+public static void main(String args[])
+{
+Base obj= new Derived();
+obj.display();
+obj.print();
+}
+}
